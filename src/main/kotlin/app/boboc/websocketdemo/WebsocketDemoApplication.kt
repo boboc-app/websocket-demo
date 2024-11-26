32b28686c37f6490bd58d95f22f1d1ecf98cc9c3
isk-bokang <bo.kang@iskra.world>
2024-11-26 18:16:18 +0900

    initial commit

---
+++ src/main/kotlin/app/boboc/websocketdemo/WebsocketDemoApplication.kt
@@ -0,0 +1,34 @@
+package app.boboc.websocketdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
+import org.springframework.web.reactive.socket.WebSocketMessage
+import org.springframework.web.reactive.socket.WebSocketSession
+import app.boboc.handler.CoroutineWebSocketHandler
+import app.boboc.util.WebSocketSessionUtil.sendMessage
+
+@SpringBootApplication
+class WebsocketDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketDemoApplication>(*args)
+}
+
+class WebfluxCoroutineHandler : CoroutineWebSocketHandler(){
+    override suspend fun coroutineHandle(session: WebSocketSession, message: WebSocketMessage) {
+        session.sendMessage("ECHO ${message.payloadAsText}")
+    }
+}
+
+@Configuration
+class WebSocketConfiguration{
+    @Bean
+    fun handlerMapping() : HandlerMapping {
+        return SimpleUrlHandlerMapping( mapOf("test" to WebfluxCoroutineHandler()), -1 )
+    }
+}
+
